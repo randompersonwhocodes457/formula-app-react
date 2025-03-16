855b723d4820d5475f76d3a13b0a0089aff25ebb
randompersonwhocodes457 <155654300+randompersonwhocodes457@users.noreply.github.com>
2025-03-16 22:07:56 +0000

    nav headings focus change

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,26 @@ import Speed from "./Speed"
 
 function App(){
 
-  const [func, setFunc] = useState("");
-  const distanceLink = document.getElementById("distance-link")
-  const speedLink = document.getElementById("speed-link")
-  const timeLink = document.getElementById("time-link")
+  const [selectedfunc, setSelectedFunc] = useState("distance");
 
-  function renderFunc(){
-    if (func === "distance"){
-      distanceLink.style.backgroundColor = "none";
-      return <Distance/>
-    } else if(func === "speed"){
-      speedLink.style.backgroundColor = "none";
-      return <Speed />
-    } else if(func === "time"){
-      timeLink.style.backgroundColor = "none";
-      return <Time />
-    } else{
-      return <Distance />
-      distanceLink.style.backgroundColor = "none";
-    }
-  }
+ function getSelectedFunc(currentFunc){
+  return currentFunc === selectedfunc ? { backgroundColor: "transparent" } : {}
+ }
 
   return(
     <div className="page">
       <nav>
         <h4>Distance, Speed and Time</h4>
         <ul className="functions">
-          <li id="distance-link"><button onClick={() => setFunc("distance")}>Distance</button></li>
-          <li id="speed-link"><button onClick={() => setFunc("speed")}>Speed</button></li>
-          <li id="time-link"><button onClick={() => setFunc("time")}>Time</button></li>
+          <li id="distance-link"><button style={{backgroundColor: "green", ...getSelectedFunc("distance")}} onClick={() => setSelectedFunc("distance")}>Distance</button></li>
+          <li id="speed-link"><button style={{backgroundColor: "blue", ...getSelectedFunc("speed")}} onClick={() => setSelectedFunc("speed")}>Speed</button></li>
+          <li id="time-link"><button style={{backgroundColor: "hsl(39, 95%, 34%)", ...getSelectedFunc("time")}} onClick={() => setSelectedFunc("time")}>Time</button></li>
         </ul>
       </nav>
       <div className="calculator">
-        {renderFunc()}
+        {selectedfunc === "distance" && <Distance />}
+        {selectedfunc === "speed" && <Speed />}
+        {selectedfunc === "time" && <Time />}
       </div>
     </div>
   )
